1abec46fa9a3c80455d95ab4a8aa365927f6bced
Aayan <aayanfaisal97@gmail.com>
2026-01-28 21:09:36 -0600

    Add kilometers to miles program

---
+++ kilo_to_miles.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+    Aayan Faisal
+    1/28/2026
+    Convert kilometers to miles using a constant conversion factor
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double kilo = 3;
+    double miles = kilo * 0.62137119;
+    cout<<"Kilo: " <<kilo<< endl;
+    cout<<"Miles: " <<miles;
+    
+    return 0;    
+}
